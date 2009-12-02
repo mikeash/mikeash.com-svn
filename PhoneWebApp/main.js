1186aa4ddaaca02c0992a69c7fde9f5bcf73cdef
mikeash <mikeash@1ea6ab17-982b-0410-b0ab-91172c90a6e5>
2009-12-02 01:56:02 +0000

    PWA: basic geolocation
    git-svn-id: file://localhost/home/mikeash/svn/mikeash@95 1ea6ab17-982b-0410-b0ab-91172c90a6e5

--- PhoneWebApp/main.js
+++ PhoneWebApp/main.js
@@ -1,6 +1,27 @@
 
+function hasNavigation()
+{
+    return (typeof navigator.geolocation != "undefined");
+}
+
+function positionWatcher(location)
+{
+    document.getElementById("longitude").firstChild.nodeValue = location.coords.longitude;
+    document.getElementById("latitude").firstChild.nodeValue = location.coords.latitude;
+}
+
 function load()
 {
     document.getElementById("loading").style.visibility = "hidden";
-    document.getElementById("loaded").style.visibility = "visible";
+    
+    if(!hasNavigation())
+    {
+        document.getElementById("nonavigation").style.visibility = "visible";
+    }
+    else
+    {
+        document.getElementById("navigation").style.visibility = "visible";
+        
+        navigator.geolocation.watchPosition(positionWatcher);
+    }
 }
