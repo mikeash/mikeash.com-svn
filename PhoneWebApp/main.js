b9365f01953c201963bcce786a61e513ba257055
mikeash <mikeash@1ea6ab17-982b-0410-b0ab-91172c90a6e5>
2009-12-03 21:44:17 +0000

    PWA: add code to detect when not loaded on a phone, and when not installed, and give the user guidance
    git-svn-id: file://localhost/home/mikeash/svn/mikeash@99 1ea6ab17-982b-0410-b0ab-91172c90a6e5

--- PhoneWebApp/main.js
+++ PhoneWebApp/main.js
@@ -14,12 +14,23 @@ function load()
 {
     document.getElementById("loading").style.visibility = "hidden";
     
-    if(!hasNavigation())
+    // no iPhone
+    if(navigator.appVersion.indexOf('iPhone OS ') < 0)
+    {
+        document.getElementById("noiphone").style.visibility = "visible";
+    }
+    else if(!window.navigator.standalone)
+    {
+        document.getElementById("notinstalled").style.visibility = "visible";
+    }
+    else if(!hasNavigation())
     {
         document.getElementById("nonavigation").style.visibility = "visible";
     }
     else
     {
+        // we're on a phone, and installed standalone
+        // "real" app init code goes here
         document.getElementById("navigation").style.visibility = "visible";
         
         navigator.geolocation.watchPosition(positionWatcher);
