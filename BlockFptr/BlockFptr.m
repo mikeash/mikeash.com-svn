d3601a761b6371e352fdf0cb203f927c25583e46
mikeash <mikeash@1ea6ab17-982b-0410-b0ab-91172c90a6e5>
2010-02-08 23:25:15 +0000

    BF: can now destroy trampolines, trampolines are cached and efficiently packed into pages
    git-svn-id: file://localhost/home/mikeash/svn/mikeash@127 1ea6ab17-982b-0410-b0ab-91172c90a6e5

--- BlockFptr/BlockFptr.m
+++ BlockFptr/BlockFptr.m
@@ -1,6 +1,7 @@
 #import "BlockFptr.h"
 
 #include <dispatch/dispatch.h>
+#include <libkern/osatomic.h>
 #include <sys/types.h>
 #include <sys/mman.h>
 
@@ -76,36 +77,90 @@ static int TrampolineAddrOffset(void)
     return addrOffset;
 }
 
-void *CreateBlockFptr(id block)
+static OSQueueHead gFptrCache = OS_ATOMIC_QUEUE_INIT;
+
+// the fptr cache actually caches the block pointer pointers
+// because the trampolines themselves are immutable
+// block pointer pointers are defined like so
+struct QueueElement
+{
+    // when in the queue, the first field is the required 'next' pointer
+    // when in use, the first field is the block pointer
+    void *nextPtrOrBlock;
+    
+    // when in the queue, the second field is to the associated trampoline
+    // when in use, the second field is unused
+    void *trampoline;
+};
+
+// dequeue an fptr, point it to the block
+// the block is copied as part of this
+static void *DequeueCachedFptr(id block)
+{
+    struct QueueElement *elt = OSAtomicDequeue(&gFptrCache, 0);
+    if(!elt)
+        return NULL;
+    
+    elt->nextPtrOrBlock = [block copy];
+    return elt->trampoline;
+}
+
+// enqueue an fptr
+// the associated block is released as part of this
+static void EnqueueCachedFptr(void *trampoline)
+{
+    struct QueueElement **eltPtr = trampoline + TrampolineAddrOffset();
+    struct QueueElement *elt = *eltPtr;
+    id block = elt->nextPtrOrBlock;
+    
+    [(id)block release];
+    
+    OSAtomicEnqueue(&gFptrCache, elt, 0);
+}
+
+void CreateNewFptrsAndEnqueue(void)
 {
     int trampolineLength = &TrampolineEnd - &Trampoline;
     int addrOffset = TrampolineAddrOffset();
     
     int pageSize = getpagesize();
-    void *trampoline = valloc(pageSize);
-    memcpy(trampoline, &Trampoline, trampolineLength);
+    int howmany = pageSize / trampolineLength;
+    void *page = valloc(pageSize);
     
-    void **blockPtr = malloc(sizeof(*blockPtr));
-    *blockPtr = [block copy];
+    struct QueueElement *elts = malloc(howmany * sizeof(*elts));
     
-    *((void **)(trampoline + addrOffset)) = blockPtr;
+    for(int i = 0; i < howmany; i++)
+    {
+        void *trampoline = page + i * trampolineLength;
+        memcpy(trampoline, &Trampoline, trampolineLength);
+        
+        elts[i].nextPtrOrBlock = NULL;
+        elts[i].trampoline = trampoline;
+        *((void **)(trampoline + addrOffset)) = &elts[i];
+    }
     
-    int err = mprotect(trampoline, pageSize, PROT_READ | PROT_EXEC);
+    int err = mprotect(page, pageSize, PROT_READ | PROT_EXEC);
     if(err)
         perror("mprotect");
     
-    return trampoline;
+    for(int i = 0; i < howmany; i++)
+    {
+        void *trampoline = page + i * trampolineLength;
+        EnqueueCachedFptr(trampoline);
+    }
+}
+
+void *CreateBlockFptr(id block)
+{
+    void *fptr;
+    while(!(fptr = DequeueCachedFptr(block)))
+        CreateNewFptrsAndEnqueue();
+    return fptr;
 }
 
 void DestroyBlockFptr(void *blockFptr)
 {
-    void *blockPtrPtr = blockFptr + TrampolineAddrOffset();
-    void *blockPtr = *(void **)blockPtrPtr;
-    void *block = *(void **)blockPtr;
-    
-    [(id)block release];
-    
-    // TODO: recycle the rest
+    EnqueueCachedFptr(blockFptr);
 }
 
 int main (int argc, const char * argv[])
