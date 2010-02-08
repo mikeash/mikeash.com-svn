34451411e92abde615948a1fa7ef9b812f98b473
mikeash <mikeash@1ea6ab17-982b-0410-b0ab-91172c90a6e5>
2010-02-08 22:41:44 +0000

    BlockFptr: general cleanup
    git-svn-id: file://localhost/home/mikeash/svn/mikeash@123 1ea6ab17-982b-0410-b0ab-91172c90a6e5

--- BlockFptr/BlockFptr.m
+++ BlockFptr/BlockFptr.m
@@ -1,54 +1,86 @@
 #import <Foundation/Foundation.h>
+
+#include <dispatch/dispatch.h>
 #include <sys/types.h>
 #include <sys/mman.h>
 
 
-void CallBlock(void)
+// for debugging purposes only
+void DumpStack(uint64_t rbp, uint64_t rsp)
 {
-    void *block = (void *)0xdeadbeefcafebabeULL;
-    void (*fptr)(void) = *(void **)(block + 16);
-    fptr();
+    uint64_t bottom = MIN(rbp, rsp) - 32;
+    uint64_t top = MAX(rbp, rsp) + 32;
+    
+    for(uint64_t ptr = top; ptr >= bottom; ptr -= 8)
+    {
+        char *marker = "        ";
+        if(ptr == rbp)
+            marker = "%rbp -->";
+        if(ptr == rsp)
+            marker = "%rsp -->";
+        fprintf(stderr, "%s 0x%016" PRIx64 ": 0x%016" PRIx64 "\n", marker, ptr, *(uint64_t *)ptr);
+    }
 }
 
 extern char Trampoline;
 extern char TrampolineEnd;
 
-void *gblockPtr;
+static int TrampolineAddrOffset(void)
+{
+    static int addrOffset;
+    static dispatch_once_t pred;
+    dispatch_once(&pred, ^{
+        uint64_t magic = 0xdeadbeefcafebabeULL;
+        for(addrOffset = 0; addrOffset <= &TrampolineEnd - &Trampoline - sizeof(uint64_t); addrOffset++)
+            if(*((uint64_t *)(&Trampoline + addrOffset)) == magic)
+                break;
+    });
+    
+    return addrOffset;
+}
+
+void *CreateBlockFptr(id block)
+{
+    int trampolineLength = &TrampolineEnd - &Trampoline;
+    int addrOffset = TrampolineAddrOffset();
+    
+    int pageSize = getpagesize();
+    void *trampoline = valloc(pageSize);
+    memcpy(trampoline, &Trampoline, trampolineLength);
+    
+    void **blockPtr = malloc(sizeof(*blockPtr));
+    *blockPtr = [block copy];
+    
+    *((void **)(trampoline + addrOffset)) = blockPtr;
+    
+    int err = mprotect(trampoline, pageSize, PROT_READ | PROT_EXEC);
+    if(err)
+        perror("mprotect");
+    
+    return trampoline;
+}
 
 int main (int argc, const char * argv[])
 {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     
     int captured = 3;
-    captured += 8;
-    id block = ^(int a, int b, int c, int d, int e, int f, int g, int h, int i) {
-        NSLog(@"3");
-        printf("hello world! %d\n", captured);
-        printf("params: %x %x %x %x %x %x %x %x %x\n", a, b, c, d, e, f, g, h, i);
-        NSLog(@"4");
+
+    id block = ^(int a, int b, int c, int d, int e) {
+        printf("hello world!\n");
+        printf("captured value is %d (should be 3)\n", captured);
+        printf("params: %x %x %x %x %x\n", a, b, c, d, e);
     };
+    
+#if 0
+    // shortcut straight into trampoline code for debugging purposes
+    static void *gblockPtr;
     gblockPtr = block;
-    
-#if 1
-    int trampolineLength = &TrampolineEnd - &Trampoline;
-    int addrOffset;
-    uint64_t magic = 0xdeadbeefcafebabeULL;
-    for(addrOffset = 0; addrOffset <= trampolineLength - sizeof(uint64_t); addrOffset++)
-        if(*((uint64_t *)(&Trampoline + addrOffset)) == magic)
-            break;
-    void *trampoline = malloc(4096 * 100);
-    memcpy(trampoline, &Trampoline, trampolineLength);
-    *((void **)(trampoline + addrOffset)) = &block;
-    
-    int err = mprotect(trampoline, trampolineLength, PROT_READ | PROT_EXEC);
-    if(err)
-        perror("mprotect");
-    NSLog(@"1 %p", trampoline);
-#else
     void *trampoline = &Trampoline;
+#else
+    void *trampoline = CreateBlockFptr(block);
 #endif
-    ((void (*)(int, int, int, int, int, int, int, int, int))trampoline)(0xdead0001, 0xdead0002, 0xdead0003, 0xdead0004, 0xdead0005, 0xdead0006, 0xdead0007, 0xdead0008, 0xdead0009);
-    NSLog(@"2");
+    ((void (*)(int, int, int, int, int))trampoline)(0xdead0001, 0xdead0002, 0xdead0003, 0xdead0004, 0xdead0005);
     
     [pool release];
     return 0;
--- BlockFptr/Trampoline.s
+++ BlockFptr/Trampoline.s
@@ -7,29 +7,26 @@
  */
 
 
+#define BLOCK_FUNCTION_POINTER_OFFSET 16
+
 .globl _Trampoline
 _Trampoline:
-//    // shuffle return address on stack down by one slot
-//    mov (%rsp), %r11
-//    mov %r11, -8(%rsp)
-//    
-//    // spill last register onto stack
-//    mov %r9, (%rsp)
-//    
-//    // shuffle stack pointer down too
-//    addq $-8, %rsp
-    
-    // move ptr-to-block-ptr into r11, dummy value is replaced at runtime
-    movabsq $0xdeadbeefcafebabe, %r11
-    //movabsq $0x100001130, %r11
-    
+    // shuffle integer argument registers down by one
+    // to make room for the implicit block ptr argument
     mov %r8, %r9
     mov %rcx, %r8
     mov %rdx, %rcx
     mov %rsi, %rdx
     mov %rdi, %rsi
+    
+    
+    // move ptr-to-block-ptr into r11, dummy value is replaced at runtime
+    movabsq $0xdeadbeefcafebabe, %r11
+    // dereference ptr-to-block-ptr, move block ptr into %rdi
     mov (%r11), %rdi
-    mov 16(%rdi), %r11
+    // extract block implementation function pointer into %r11
+    mov BLOCK_FUNCTION_POINTER_OFFSET(%rdi), %r11
+    // jump to block implementation
     jmp *%r11
 .globl _TrampolineEnd
 _TrampolineEnd:
