f74ebfcd4f8d4840e6bc047a952c7812023de634
mikeash <mikeash@1ea6ab17-982b-0410-b0ab-91172c90a6e5>
2011-08-09 15:32:38 +0000

    MAFuture: remove all code, replace with readme pointing at GitHub
    git-svn-id: file://localhost/home/mikeash/svn/mikeash@221 1ea6ab17-982b-0410-b0ab-91172c90a6e5

--- MAFuture/MAFuture.h
+++
@@ -1,6 +0,0 @@
-
-id MABackgroundFuture(id (^block)(void));
-id MALazyFuture(id (^block)(void));
-
-#define MABackgroundFuture(...) ((__typeof((__VA_ARGS__)()))MABackgroundFuture((id (^)(void))(__VA_ARGS__)))
-#define MALazyFuture(...) ((__typeof((__VA_ARGS__)()))MALazyFuture((id (^)(void))(__VA_ARGS__)))
---
+++ MAFuture/readme.txt
@@ -0,0 +1,5 @@
+MAFuture has moved! It is now hosted on GitHub. You can view it here:
+
+https://github.com/mikeash/MAFuture
+
+The code that was hosted here is out of date and the GitHub version is now the official and most up-to-date copy of the code.
